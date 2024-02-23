08eb62d9b5f9c929f7bce0aed0e304b4bfab1547
unknown <aguit.inan@gmail.com>
2024-02-23 09:28:25 +0100

    feat: update Profile.cs

---
+++ CoVoyageurCore/Models/Profile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using CoVoyageurCore.Models;
+
+namespace CoVoyageurCore.Models
+{
+    [Table("profile")]
+    public class Profile
+    {
+        //[Column("id")]
+        public int Id { get; set; }
+
+        //[Column("email")]
+        //[Required]
+        public string? Email { get; set; }
+
+        //[Column("rating")]
+        //[Required]
+        public int? Rating { get; set; }
+
+        //[Column("review")]
+        //[Required]
+        public string? Review { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public List<Car> Cars { get; set; }
+
+        public Preference? Preferences { get; set; }
+
+        public enum Preference
+        {
+            Musique,
+            Tabac,
+            Animaux
+        }
+
+    }
+}
