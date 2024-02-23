b0614fb763b423f2372a225395c884134e077a99
unknown <aguit.inan@gmail.com>
2024-02-23 09:28:46 +0100

    feat: update Car.cs

---
+++ CoVoyageurCore/Models/Car.cs
@@ -0,0 +1,37 @@
+﻿using CoVoyageurCore.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoVoyageurCore.Models
+{
+    [Table("car")]
+    public class Car
+    {
+        //[Column("id")]
+        public int Id { get; set; }
+
+        //[Column("license_plate")]
+        //[Required]
+        public string? LicensePlate { get; set; }
+
+        //[Column("model")]
+        //[Required]
+        public string? Model { get; set; }
+
+        //[Column("brand")]
+        //[Required]
+        public string? Brand { get; set; }
+
+        //[Column("color")]
+        //[Required]
+        public string? Color { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+    }
+}
