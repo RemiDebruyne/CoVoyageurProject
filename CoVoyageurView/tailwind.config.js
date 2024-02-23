d1a7fa27e1465f437c6850814fb314ea6bb2cc23
Remi Debruyne <remi@remi-laptop>
2024-02-23 11:53:42 +0100

    build: install tailwind, remove boostrap

---
+++ CoVoyageurView/tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+        content: ["./**/*.razor", "./wwwroot.index.html"],
+    theme: {
+        extend: {
+            colors: {
+                'primary': {
+                    50: '#e9faf5',
+                    100: '#baefde',
+                    200: '#98e8cf',
+                    300: '#6addb9',
+                    400: '#4dd6ab',
+                    500: '#20cc96',
+                    600: '#1dba89',
+                    700: '#17916b',
+                    800: '#127053',
+                    900: '#0d563f',
+                },
+
+            },
+        },
+        plugins: [],
+    }
+}
