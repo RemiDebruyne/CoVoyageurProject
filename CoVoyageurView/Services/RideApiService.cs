765b450337b3e5570e3fe5ead3b1f6efa1612f45
Remi Debruyne <remidebruyne@hotmail.com>
2024-03-08 16:53:21 +0100

    Add car and ride services

---
+++ CoVoyageurView/Services/RideApiService.cs
@@ -0,0 +1,13 @@
+﻿using CoVoyageurCore.Models;
+
+namespace CoVoyageurView.Services
+{
+    public class RideApiService : GenericApiQueryService<Ride>
+    {
+        public string Controller = "ride";
+
+        public RideApiService(HttpClient httpClient) : base(httpClient)
+        {
+        }
+    }
+}
