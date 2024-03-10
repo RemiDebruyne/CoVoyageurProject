ec120ba18baa1f4a37153410d815a985d565059d
remiD <remidebruyne@hotmail.com>
2024-03-10 22:07:18 +0100

    feat(register): add request to the api
    Ongoing work

--- CoVoyageurView/Program.cs
+++ CoVoyageurView/Program.cs
@@ -10,13 +10,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
+builder.Services.AddScoped<AuthenticationApiService>();
 builder.Services.AddScoped<IApiQueryService<User>, UserApiService>();
 builder.Services.AddScoped<IApiQueryService<Profile>, ProfileApiService>();
 builder.Services.AddScoped<IApiQueryService<Car>,CarApiService>();
 builder.Services.AddScoped<IApiQueryService<Ride>,RideApiService>();
-builder.Services.AddSingleton<FakeDb>();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddSingleton<FakeDb>();
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5199/api/") });
 
 await builder.Build().RunAsync();
-
