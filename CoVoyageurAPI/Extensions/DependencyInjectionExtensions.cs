a325356c95b4bf2523612194504912aa604ae8f0
kAly-59 <kev.callet@gmail.com>
2024-03-11 15:24:33 +0100

    update Dependency

--- CoVoyageurAPI/Extensions/DependencyInjectionExtensions.cs
+++ CoVoyageurAPI/Extensions/DependencyInjectionExtensions.cs
@@ -37,7 +37,7 @@ namespace CoVoyageurAPI.Extensions
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzApi", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CoVoyageurAPI", Version = "v1" });
                 c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
                 {
                     In = ParameterLocation.Header,
